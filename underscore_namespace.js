950fe50fb4eedbeb6e974717114194e91531f88e
Eduardo Turiño <eturino@artirix.com>
2014-04-27 15:11:31 +0100

    first commit

---
+++ underscore_namespace.js
@@ -0,0 +1,17 @@
+// based on namespace function from projs course (https://github.com/redradix/material-projs/)
+(function (_) {
+    _.mixin({
+        namespace: function (string, rootObj, sandboxFn) {
+            var spaces = string.split('.'),
+                root = rootObj || window,
+                f = sandboxFn || function (x) { return x },
+                space;
+            
+            while (space = spaces.shift()) {
+                root = root[space] || (root[space] = {});
+            }
+            return f(root);
+        }
+    });
+})(_);
+
